0b39ce87b0b31c8066e68e558da4c69fef01c35c
Michael Leow <leow@sinarproject.org>
2017-04-01 07:41:27 +0800

    git subrepo clone https://github.com/laracasts/Lets-Build-a-Forum-in-Laravel.git build-forum
    subrepo:
      subdir:   "build-forum"
      merged:   "bb8a4b7"
    upstream:
      origin:   "https://github.com/laracasts/Lets-Build-a-Forum-in-Laravel.git"
      branch:   "master"
      commit:   "bb8a4b7"
    git-subrepo:
      version:  "0.3.0"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "988f8c8"

---
+++ build-forum/app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Channel;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \View::composer('*', function ($view) {
+            $view->with('channels', Channel::all());
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
